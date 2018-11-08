bc78e625106a767aabf279d83be29296becaec81
Ildar Lomov <ildar.lomov@ostrovok.ru>
2018-11-08 14:36:46 +0300

    switch to videofile

--- test/facial_landmark_detection.py
+++ test/facial_landmark_detection.py
@@ -3,6 +3,8 @@ from __future__ import division
 import dlib
 import cv2
 import numpy as np
+from stuff.helper import FPS2
+
 
 def resize(img, width=None, height=None, interpolation=cv2.INTER_AREA):
     global ratio
@@ -21,6 +23,7 @@ def resize(img, width=None, height=None, interpolation=cv2.INTER_AREA):
         resized = cv2.resize(img, (height, width), interpolation)
         return resized
 
+
 def shape_to_np(shape, dtype="int"):
     # initialize the list of (x, y)-coordinates
     coords = np.zeros((68, 2), dtype=dtype)
@@ -33,13 +36,22 @@ def shape_to_np(shape, dtype="int"):
     # return the list of (x, y)-coordinates
     return coords
 
-camera = cv2.VideoCapture(1)
+
+# camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture('data/video_file2.mp4')
 
 predictor_path = 'shape_predictor_68_face_landmarks.dat_2'
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 
+# Visualize Text on OpenCV Image
+def vis_text(image, string, pos):
+    cv2.putText(image, string, (pos),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (77, 255, 9), 2)
+
+FPS_INTERVAL = 5
+fps = FPS2(FPS_INTERVAL).start()
 while True:
 
     ret, frame = camera.read()
@@ -64,12 +76,15 @@ while True:
             # loop over the (x, y)-coordinates for the facial landmarks
             # and draw them on the image
             for (x, y) in shape:
-                cv2.circle(frame, (int(x/ratio), int(y/ratio)), 3, (255, 255, 255), -1)
-            #cv2.rectangle(frame, (int(d.left()/ratio), int(d.top()/ratio)),(int(d.right()/ratio), int(d.bottom()/ratio)), (0, 255, 0), 1)
-
+                cv2.circle(frame, (int(x / ratio), int(y / ratio)), 3, (255, 255, 255), -1)
+            cv2.rectangle(frame, (int(d.left()/ratio), int(d.top()/ratio)),(int(d.right()/ratio), int(d.bottom()/ratio)), (0, 255, 0), 1)
+    vis_text(frame, "fps: {}".format(fps.fps_local()), (10, 30))
+    # print('fps:', fps.fps_local())
     cv2.imshow("image", frame)
+    fps.update()
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        fps.stop()
         cv2.destroyAllWindows()
         camera.release()
         break
